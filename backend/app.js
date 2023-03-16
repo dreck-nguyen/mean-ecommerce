fa9c376fc08e5c8cd3ba8284c141e1ca7f3de0fe
DUCHNH <hduy01012000@gmail.com>
2023-03-16 15:58:09 +0700

    add env file for environment

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+require('dotenv/config');
+
+const api = process.env.API_URL
+
+
+app.get(api + '/', (req, res) => {
     res.send('hello API !')
 })
 
 app.listen(3000, () => {
+    console.log(api);
     console.log('server is running http://localhost:3000');
 })
