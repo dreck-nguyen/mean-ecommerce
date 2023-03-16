61029155f1fff620663cbea254df8157a87863f7
DUCHNH <hduy01012000@gmail.com>
2023-03-16 18:20:09 +0700

    create model and schema

--- backend/app.js
+++ backend/app.js
@@ -2,57 +2,24 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const productsRouter = require('./routers/products')
+const api = process.env.API_URL
 
-const dbMain = 'eshop-database'
+require('dotenv').config()
+// require('dotenv/config')
+
+// const dbMain = 'eshop-database'
 const dbTest = 'mean-shop'
 const dbNameStr = dbTest;
 
-
-require('dotenv/config')
 // Middleware
 app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
 app.use(morgan('tiny'))
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
+// Routers
+app.use(`${api}/products`, productsRouter)
 
-const Product = mongoose.model('Product', productSchema)
-
-const api = process.env.API_URL
-
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url'
-    }
-    res.send(product)
-})
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    product.save()
-        .then((createdProduct) => {
-            // res.send(product)
-            return res.status(201).json(createdProduct)
-        })
-        .catch((err) => {
-            return res.status(500).json({
-                error: err,
-                success: false,
-                message: 'Save error'
-            })
-        });
-})
-
+//MongoDb Connection
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -64,6 +31,8 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     .catch((err) => {
         console.log(err);
     })
+
+// Server Listening
 app.listen(3000, () => {
     console.log(api);
     console.log('server is running http://localhost:3000');
