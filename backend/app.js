8816ec3840939d646e62448cdf41cb1397cb1cc3
DUCHNH <hduy01012000@gmail.com>
2023-03-16 16:14:49 +0700

    add mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-require('dotenv/config');
+require('dotenv/config')
+// Middleware
+app.use(express.json())
+app.use(morgan('tiny'))
 
 const api = process.env.API_URL
 
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product)
+})
 
-app.get(api + '/', (req, res) => {
-    res.send('hello API !')
+app.post(`${api}/products`, (req, res) => {
+    const product = req.body
+    console.log(product);
+    res.send(product)
 })
 
 app.listen(3000, () => {
